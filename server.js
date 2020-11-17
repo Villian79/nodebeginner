a007d81dbe0c170a9716765e0c6f4f8a1da22e90
Villian79 <igor.vilinchuk@multiloginapp.com>
2020-11-17 22:25:55 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+function start() {
+  http
+    .createServer((reqest, response) => {
+      console.log("Request received");
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write("Hello World");
+      response.end();
+    })
+    .listen(8888);
+
+  console.log("Server started");
+}
+
+exports.start = start;
